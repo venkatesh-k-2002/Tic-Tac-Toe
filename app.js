7e089ca27d3756f2282fe4a74918284f0bd6c0b9
Venkatesh P. Khatake <157818525+venkatesh-k-2002@users.noreply.github.com>
2024-02-08 17:50:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let newGameBtn = document.querySelector("#new-game-button");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+
+        }
+        else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+
+const checkWinner = () => {
+    for (let patterns of winPatterns) {
+        let pos1Val = boxes[patterns[0]].innerText;
+        let pos2Val = boxes[patterns[1]].innerText;
+        let pos3Val = boxes[patterns[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("Winner", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
